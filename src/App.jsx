cfb95728c2439436fa73075b3e8aa869695aa2d1
Natalia Razmadze <natasha.razmadze@gmail.com>
2025-06-13 22:43:31 -0700

    Applied changes for wave 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import './App.css';
+import ChatEntry from './components/ChatEntry';
+import chatMessages from './data/messages.json';
 
 const App = () => {
+  const exampleMessage = chatMessages[0];
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        <ChatEntry
+          sender={exampleMessage.sender}
+          body={exampleMessage.body}
+          timeStamp={exampleMessage.timeStamp}
+        />
       </main>
     </div>
   );
